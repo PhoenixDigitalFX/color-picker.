a16830f12c4c7431620f216ce3f51661fdfc5c21
Amelie Fondevilla <amelie@les-fees-speciales.coop>
2022-02-18 17:06:22 +0100

    reorganized settings

--- gpcolorpicker/picker_settings.py
+++ gpcolorpicker/picker_settings.py
@@ -2,17 +2,37 @@ import numpy as np
 from math import floor, ceil, pi, sin, asin
 
 class GPCOLORPICKER_settings():
-    def __init__(self): 
-        self.key_shortcut = 'A'
+    def __init__(self, prefs = None):
+        # Const values
         self.switch_key = 'TAB'
-        self.origin = np.asarray([0,0])
-        self.active_obj = None
-        self.materials = []
-        self.region_dim = np.asarray([0,0])
+        self.active_color =  (0.05,0.05,0.05,1)
+        self.mat_line_width = 5.
+        self.mc_line_width = 1.
         self.anti_aliasing_eps = 0.5
 
-        self.mat_from_active = True
-        self.mat_assign = False
+        # From user preferences
+        if prefs:
+            self.mat_from_active = (prefs.mat_mode == "from_active")
+            self.mat_assign = prefs.assign_mat
+            self.mc_fill_color = prefs.theme.pie_color
+            self.mc_line_color = prefs.theme.line_color
+            self.text_color = prefs.theme.text_color
+            self.set_icon_scale(prefs.icon_scale)
+        else:
+            self.mat_from_active = True
+            self.mat_assign = False
+            self.mc_fill_color = (0.4,0.4,0.4,1.)
+            self.mc_line_color = (0.96,0.96,0.96,1.)
+            self.text_color = (0.,0.,0.,1.)
+            self.set_icon_scale(250)
+
+        # OLD
+        self.origin = np.asarray([0,0])
+        self.region_dim = np.asarray([0,0])
+
+        # context 
+        self.active_obj = None
+        self.materials = []
         self.mat_nb = -1
         self.mat_selected =  -1
         self.mat_active =  -1
@@ -20,18 +40,10 @@ class GPCOLORPICKER_settings():
         self.mat_line_colors = []
         self.custom_angles = []
 
+        # cached
         self.cached_gpu_tex = None
         self.cached_mat_selected = -1
         self.cached_palette_name = ""
-
-        self.mc_fill_color = (0.4,0.4,0.4,1.)
-        self.mc_line_color = (0.96,0.96,0.96,1.)
-        self.active_color =  (0.05,0.05,0.05,1)
-        self.text_color = (0.,0.,0.,1.)
-
-        self.mat_line_width = 5.
-        self.mc_line_width = 1.
-        self.set_icon_scale(250)
 
     def set_icon_scale(self,scale):
         self.icon_scale = scale
