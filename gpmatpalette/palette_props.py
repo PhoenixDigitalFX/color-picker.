d559383f74c15b49f4314102c7be13a126c3920e
Amelie Fondevilla <amelie@les-fees-speciales.coop>
2022-03-10 09:51:15 +0100

    documentation palette props

--- gpmatpalette/palette_props.py
+++ gpmatpalette/palette_props.py
@@ -5,47 +5,62 @@ from bpy.props import *
 import numpy as np
 from . palette_maths import get_unset_intervals
 
-''' Position of a material in the picker'''
+''' --- Material Item --- '''
 class GPMatItem(PropertyGroup):
-    name: StringProperty(name= "Material name")
+    name: StringProperty(name= "Name")
 
-    image: PointerProperty(type=bpy.types.Image, name="Material palette image")
-    layer: StringProperty(name="Material layers")
+    image: PointerProperty(type=bpy.types.Image, name="Image", description="Image to be displayed in the picker when material is in selection")
+    layer: StringProperty(name="Layer", description="Layer to switch to when material is selected")
 
-    angle: FloatProperty(subtype="ANGLE", default=-1, name="Material position around the wheel")
-    is_angle_movable: BoolProperty(default=True, name="Is position computed by default or set manually")
+    angle: FloatProperty(name="Angle", description="Material angle position in the picker", subtype="ANGLE", default=-1)
+    is_angle_movable: BoolProperty(name="Movable", description="The angle is computed dynamically",default=True)
 
-    has_pick_line: BoolProperty(default=False, name="Material has pickline")
-    ox : FloatProperty(default=0, name="Pickline origin x coordinate")
-    oy : FloatProperty(default=0, name="Pickline origin y coordinate")
+    has_pick_line: BoolProperty(name="Has Pickline", default=False)
+    ox : FloatProperty(name="Origin X", default=0, description="Pickline origin x coordinate")
+    oy : FloatProperty(name="Origin Y", default=0, description="Pickline origin y coordinate")
 
+    ''' Clear all material item data '''
     def clear(self):
-        pass
+        self.image = None
     
+    ''' Getter for angle property '''
     def get_angle(self, only_if_not_movable = False):
         if only_if_not_movable and self.is_angle_movable:
             return -1
         return self.angle
-    
+
+    ''' Setter for angle property'''
     def set_angle(self, a, auto=False):
         self.angle = a
         self.is_angle_movable = auto
 
+    ''' Setter for pickline origin (both x and y coordinates)'''
     def set_origin(self, origin, auto=False):
         self.ox = origin[0]
         self.oy = origin[1]
         self.has_pick_line = not auto
     
+    ''' Getter for pickline origin (both x and y coordinates)
+        If with_bool is set to True, an additionnal 3D coord will be added
+        indicating whether the pickline exists or not
+    '''
     def get_origin(self, with_bool = False):
         if with_bool:
             return [self.ox, self.oy, self.has_pick_line]
         return [self.ox, self.oy]
 
+''' --- Palette --- '''
+
+''' Update callback for the Image property
+'''
 def update_im(self, context):
     self.is_dirty = True
     if self.image:
         self.image.pack()
 
+''' Update callback for the is_obsolete property
+    We use a lock to prevent direct modification in the UI
+'''
 def update_with_lock(self, context):
     if self.is_obsolete != self.lock_obsolete:
         self.is_obsolete = self.lock_obsolete
@@ -53,7 +68,7 @@ class GPMatPalette(PropertyGroup):
     bl_idname= "scene.gpmatpalettes.palette"
 
     name: StringProperty(name="Name", default="unnamed")
-    materials: CollectionProperty(name = "Materials", type=GPMatItem)
+    materials: CollectionProperty(name = "Materials", description="List of materials sorted by angle", type=GPMatItem)
     image: PointerProperty(name="Image", type=bpy.types.Image, update=update_im)
     visible: BoolProperty(name="Visible", default=True)
 
@@ -67,6 +82,9 @@ class GPMatPalette(PropertyGroup):
     
     pending_material: PointerProperty(type=bpy.types.Material)
 
+    ''' Automatic completion of material angle positions
+        useful when some are not specified
+    '''
     def autocomp_positions(self):
         angles = [(m.get_angle(True), i) for i,m in enumerate(self.materials)]
         angles_to_set = get_unset_intervals(angles)
@@ -85,6 +103,9 @@ class GPMatPalette(PropertyGroup):
             a = m.get_angle()
             m.set_origin([math.cos(a), math.sin(a)], auto=True)
     
+    ''' Get material index from angle position
+        Useful to insert a material at a certain angle position
+    '''
     def get_index_by_angle(self, angle):
         ind = 0
         for m in self.materials:
@@ -94,12 +115,17 @@ class GPMatPalette(PropertyGroup):
             ind += 1
         return ind
     
+    ''' Insert a new material at a given angle position
+    '''
     def set_material_by_angle(self, name, angle, auto=False):
         ind = self.get_index_by_angle(angle)
         matit = self.set_material(name, ind)
         matit.set_angle(angle, auto)
         return matit
 
+    ''' Insert a new material at a given collection index
+        If index is unspecified or < 0, the material will be inserted at the end of the collection
+    '''
     def set_material(self, name, index = -1):
         old_id = self.count()
         if name in self.materials:
@@ -114,28 +140,35 @@ class GPMatPalette(PropertyGroup):
         self.autocomp_positions()
         return self.materials[name]
     
-
+    ''' Remove a material from the collection (given by collection index) '''
     def remove_material(self, ind):
         self.materials.remove(ind)
         self.is_dirty = True
 
+    ''' Clearing all palette data '''
     def clear(self):
         for m in self.materials:
             m.clear()
         self.materials.clear()
         self.image = None
-
+    
+    ''' Number of materials in collection '''
     def count(self):
         return len(self.materials)
     
+    ''' Can a material be added to collection '''
     def is_material_available(self, mat):
         if (not mat) or (not mat.is_grease_pencil):
             return False
         return not mat.name in self.materials
     
+    ''' Timestamp comparison function '''
     def is_same_timestamp(self, other_tmstp):
         return (self.timestamp == other_tmstp)
 
+    ''' Adds the pending material in the collection
+        Useful in palette editor on addmaterial operator
+    '''
     def accept_pending_material(self, angle=-1):
         if not self.is_material_available(self.pending_material):
             self.pending_material = None
@@ -149,6 +182,9 @@ class GPMatPalette(PropertyGroup):
         self.is_dirty = True
         return True
 
+    ''' Setter for the is_obsolete property
+        We use a lock to prevent direct modification in the UI
+    '''
     def set_obsolete(self, val):
         self.lock_obsolete = val
         self.is_obsolete = self.lock_obsolete
@@ -172,8 +208,8 @@ class GPMatPalettes(PropertyGroup):
     palettes: CollectionProperty(name= "Palettes", type=GPMatPalette)
     active_index: IntProperty(name="Active palette index", default=-1, update=update_palette_active_index)
 
-    is_dirty: BoolProperty(name="Is Collection Dirty", description="True if the palette collection was modified recently, False otherwise", default=False)
-    is_obsolete: BoolProperty(name="Is Collection Obsolete", description="True if the palette collection is based on obsolete files, False otherwise", default=False)
+    is_dirty: BoolProperty(name="Dirty", description="The palette collection was modified recently", default=False)
+    is_obsolete: BoolProperty(name="Obsolete", description="The palette collection is based on obsolete files", default=False)
 
     mem_dir: IntProperty(name="Mem dir", description="Last direction of navigation between palettes", default=1)
 
